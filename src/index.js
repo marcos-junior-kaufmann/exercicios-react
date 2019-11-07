044c0ca658734b5f19758ee5d6f751af5df5f181
Marcos Junior Kaufmann <marcos.junior.kaufmann@gmail.com>
2019-11-07 19:19:53 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PrimeiroComponente from './componentes/PrimeiroComponente'
+
+ReactDOM.render(<div><PrimeiroComponente></PrimeiroComponente></div>, document.getElementById('root'));
+
+// ReactDOM.render(<h1>Bem Vindo</h1>, document.getElementById('root'));
