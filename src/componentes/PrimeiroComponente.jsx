79b80d1d65a58a0d10b52f54f3b4fe276ea84aa0
Marcos Junior Kaufmann <marcos.junior.kaufmann@gmail.com>
2019-11-12 16:21:41 -0300

    parametro - props

--- src/componentes/PrimeiroComponente.jsx
+++ src/componentes/PrimeiroComponente.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
+export default (props) =>
+	<div><h1>{props.valor}</h1></div>
 
-export default () =>
-	<div><h1>Primeiro Componente (Arrow)</h1></div>
-
+// export default () =>
+// 	<div><h1>Primeiro Componente (Arrow)</h1></div>
 
 // export default function () {
 // 	return <h1>Primeiro Componente</h1>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PrimeiroComponente from './componentes/PrimeiroComponente'
 
-ReactDOM.render(<div><PrimeiroComponente></PrimeiroComponente></div>, document.getElementById('root'));
+ReactDOM.render(<PrimeiroComponente valor="Bom dia!" />, document.getElementById('root'));
+
+// ReactDOM.render(<PrimeiroComponente/>, document.getElementById('root'));
 
 // ReactDOM.render(<h1>Bem Vindo</h1>, document.getElementById('root'));
